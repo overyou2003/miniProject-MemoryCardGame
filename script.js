05105fe5bbb01c5d276d21a37e7579aa837bab96
overyou2003 <s6506021620032@email.kmutnb.ac.th>
2025-09-04 23:23:45 +0700

    commit

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded' , () => {
+    const cards = document.querySelectorAll('.card')
+
+
+    cards.forEach(card => {
+    
+        card.addEventListener('click' , flipCard)
+    })
+
+    
+    let cardOne , cardTwo
+    function flipCard(e) {
+        let clickedCard = e.target
+        if(clickedCard !== cardOne) {
+            clickedCard.classList.add('flip')
+            if(!cardOne) {
+                return cardOne = clickedCard;
+            }
+            cardTwo = clickedCard
+            let cardOneImg = cardOne.querySelector("img").src
+            let cardTwoImg = cardTwo.querySelector("img").src
+
+            matchCards(cardOneImg , cardTwoImg)
+        } 
+    }
+
+    function matchCards(img1 , img2) {
+        if (img1 === img2) {
+            return console.log('card matched!')
+        }
+        return console.log('card not matched!')
+    }
+})
